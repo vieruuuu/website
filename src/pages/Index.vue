460c06d6c054ec2d51b8fb2e9d82ac40d1e2f137
vieru <mixerandr3w001@gmail.com>
2021-05-06 21:46:56 +0300

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="bg-grey-10 flex flex-center">
+    <div class="text-yellow-5 text-center">
+      <h2>Vieru Andrei</h2>
+      <h5>Javascript Developer</h5>
+    </div>
+  </q-page>
+</template>
+
+<script>
+// TODO:
+// HOME
+// ABOUT
+// SERVICII/CE STIU
+// EXEMPLE/PROIECTE
+// CONTACT
+
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageIndex",
+});
+</script>
